ed54eaa5beec446e341313a4d22b2d0cab139273
Julio Eduardo B. Livelo <94818606+yongLivelo@users.noreply.github.com>
2024-09-18 22:32:48 +0800

    Added Features

--- src/script.js
+++ src/script.js
@@ -1,96 +1,125 @@
 class Query {
   static queries = [];
-  constructor(amount, flow, payment, expense, date) {
-    this.amount = amount;
-    this.flow = flow;
-    this.payment = payment;
-    this.expense = expense;
+  constructor(date, flow, mode, tags, amount, description) {
+    this.code = Query.createCode(flow);
     this.date = date;
+    this.flow = flow;
+    this.mode = mode;
+    this.tags = tags;
+    this.amount = amount;
+    this.description = description;
+  }
+
+  static createCode(flow) {
+    let flowArr = Query.queries.filter((query) => {
+      return query.flow === flow;
+    });
+
+    return flowArr.length + 1;
   }
 
   render() {
     let stringTable = "";
     let totalAmount = 0;
-    let personalTotalAmount = 0;
-    let businessTotalAmount = 0;
 
     Query.queries.forEach((query) => {
-      stringTable += `<tr><td>${query.amount}</td><td>${query.flow}</td><td>${
-        query.payment
-      }</td><td>${
-        query.expense
-      }</td><td>${query.date.getHours()}:${query.date.getMinutes()}</td></tr>`;
+      let tagsString = "";
+      query.tags.forEach((value) => {
+        tagsString += `<p class="rounded bg-secondary p-2">${value}</p>`;
+      });
+      stringTable += `<tr><td>${query.code}</td><td>${query.date}</td><td>${query.flow}</td><td>${query.mode}</td><td>${tagsString}</td><td>${query.amount}</td><td>${query.description}</td></tr>`;
 
-      if (query.flow === "out") {
+      if (query.flow === "Expenses") {
         totalAmount -= query.amount;
-      } else {
+      } else if (query.flow === "Sales") {
         totalAmount += query.amount;
-      }
-
-      if (query.expense === "Personal expense") {
-        if (query.flow === "out") {
-          personalTotalAmount -= query.amount;
-        } else {
-          personalTotalAmount += query.amount;
-        }
-      } else if (query.expense === "Business expense") {
-        if (query.flow === "out") {
-          businessTotalAmount -= query.amount;
-        } else {
-          businessTotalAmount += query.amount;
-        }
       }
     });
 
-    totalAmountEl.innerHTML = "₱" + totalAmount;
-    personalTotalAmountEl.innerHTML = "₱" + personalTotalAmount;
-    businessTotalAmountEl.innerHTML = "₱" + businessTotalAmount;
-
-    queryTable.innerHTML = stringTable;
+    tableQuery.innerHTML = stringTable;
+    totalAmountLabel.innerHTML = "₱" + totalAmount;
   }
 }
 
 const submitQuery = document.getElementById("submit-query");
-const amountLabel = document.getElementById("amount");
+const dateLabel = document.getElementById("date-label");
 const flowLabel = document.getElementsByName("flow");
-const paymentLabel = document.getElementById("payment");
-const expenseLabel = document.getElementById("expense");
-const queryTable = document.getElementById("query-table");
-const totalAmountEl = document.getElementById("total-amount");
-const personalTotalAmountEl = document.getElementById("personal-total-amount");
-const businessTotalAmountEl = document.getElementById("business-total-amount");
+const modeLabel = document.getElementById("mode-label");
+const tagContainer = document.getElementById("tag-container");
+const amountLabel = document.getElementById("amount-label");
+const descriptionLabel = document.getElementById("description-label");
+const tableQuery = document.getElementById("table-query");
+const totalAmountLabel = document.getElementById("total-amount-label");
+
+let flowValue = "Sales";
+flowLabel.forEach((flow) => {
+  flow.addEventListener("click", (input) => {
+    if (flow.checked) {
+      flowValue = flow.id;
+    }
+    // Hardcoded for now
+    if (flowValue === "Expenses") {
+      tagContainer.innerHTML = `
+      <select id="mode-label" class="form-select">
+      <option value="0" selected>Chart of Accounts</option>
+      <option value="Office Supplies">Office Supplies</option>
+      <option value="Cost of Goods">Cost of Goods</option>
+      <option value="Delivery">Delivery</option></select>`;
+    } else if (flowValue === "Sales") {
+      tagContainer.innerHTML = `
+      <select id="mode-label" class="form-select">
+      <option value="0" selected>Business Account</option>
+      <option value="Bikeshop ni JE">Bikeshop ni JE</option>
+      <option value="JE Bikeshop">JE Bikeshop</option></select>
+
+      <select id="mode-label" class="form-select">
+      <option value="0" selected>Source</option>
+      <option value="Web Store">Web Store</option>
+      <option value="Store">Store</option>
+      <option value="Tiktok">Tiktok</option>
+      <option value="Whole Sale">Whole Sale</option></select>`;
+    }
+  });
+});
 
 submitQuery.addEventListener("click", (event) => {
+  event.preventDefault();
+  const values = [...tagContainer.children].map((flow) => {
+    return flow.options[flow.selectedIndex].value;
+  });
   if (
-    amountLabel.value === undefined ||
-    paymentLabel.options[paymentLabel.selectedIndex].text ===
-      "Select payment type" ||
-    expenseLabel.options[expenseLabel.selectedIndex].text ===
-      "Select expense type"
+    dateLabel.value === " " ||
+    modeLabel.options[modeLabel.selectedIndex].value === "0" ||
+    amountLabel.value === 0 ||
+    values.includes("0")
   ) {
-    alert("Input missing");
+    alert("Missing Input");
   } else {
-    let flowValue;
-    flowLabel.forEach((input) => {
-      if (input.checked) {
-        flowValue = input.value;
-      }
-    });
-
-    let date = new Date();
-    event.preventDefault();
-
     let query = new Query(
-      parseFloat(amountLabel.value),
+      dateLabel.value,
       flowValue,
-      paymentLabel.options[paymentLabel.selectedIndex].text,
-      expenseLabel.options[expenseLabel.selectedIndex].text,
-      date
+      modeLabel.options[modeLabel.selectedIndex].value,
+      values,
+      parseInt(amountLabel.value),
+      descriptionLabel.value === "" ? "N/A" : descriptionLabel.value
     );
+
     Query.queries.push(query);
     query.render();
   }
 });
 
-const blurredText = document.querySelectorAll(".blurred");
-console.log(blurredText);
+const saveSettings = document.getElementById("save-settings");
+saveSettings.addEventListener("click", () => {});
+
+const saveTable = document.getElementById("save-table");
+saveTable.addEventListener("click", () => {
+  localStorage.setItem("files", JSON.stringify(Query.queries));
+});
+
+const loadTable = document.getElementById("load-table");
+loadTable.addEventListener("click", () => {
+  const retreieve = localStorage.getItem("files");
+  const query = new Query();
+  query.render();
+});
